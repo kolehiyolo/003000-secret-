df4c1c74beafa52b9763a452af5f3af774d69e91
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-05-27 15:32:50 +0800

    Registration Working
    1. Downloaded and installed Mongoose NPM
    2. Setup a typical MongoDB+Mongoose connection via Node.js
    3. Created a new model+schema "User"
    4. Created a POST /register route for when the user submits the registration form
    5. Added proper loggin in each route for better tracking in the console
    6. I actually ran into an issue with the POST /register route where the insertOne wasn't working
    	6.1. The problem was two-fold, where first I forgot to write "User.collection.insertOne" and instead did "User.insertOne"
    	6.2. The second is, Angela's way of establishing the mongoose connection is outdated
    	6.3. See line 19-22 for the updated way of doing things

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const ejs = require(`ejs`);
+const mongoose = require(`mongoose`);
 
 // * Setting up Express
 const app = express(); // Setting up app head
@@ -14,26 +15,70 @@ app.use(bodyParser.urlencoded({
 const port = 3000; // Port number; Adjustable depending on eventual hosting config
 
 
+// * Setting up MongoDB
+mongoose.connect(`mongodb://127.0.0.1:27017/secretsDB`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); // Establishing connection to local MongoDB instance
+const User = new mongoose.model(`User`, {
+    email: String,
+    password: String
+}); // Creating a new collection "User" using the indicated schema
+
 // * EXPRESS ROUTES
 // -* GET Home
 app.get(`/`, function(req, res) {
+    console.log(`\n`); 
     console.log(`GET /`); 
+    console.log(`-Rendering home page`); 
 
     res.render(`home`);
 }) ; 
 
 // -* GET Login
 app.get(`/login`, function(req, res) {
+    console.log(`\n`); 
     console.log(`GET /login`); 
+    console.log(`-Rendering login page`); 
 
     res.render(`login`);
 }) ; 
 
 // -* GET Register
 app.get(`/register`, function(req, res) {
+    console.log(`\n`); 
     console.log(`GET /register`); 
+    console.log(`-Rendering register page`); 
 
     res.render(`register`);
+}) ; 
+
+// -* POST Register
+app.post(`/register`, function(req, res) {
+    console.log(`\n`); 
+    console.log(`POST /register`); 
+    console.log(`-Processing user registration`); 
+    console.log(`-Creating new user based on inputs`); 
+
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    
+    console.log(`-New user created:`); 
+    console.log(newUser); 
+
+    console.log(`-Inserting new user to "User" collection`); 
+    User.collection.insertOne(newUser, (err)=>{
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`); 
+            console.log(err); 
+        } else {
+            console.log(`-Insert successful`); 
+            console.log(`-Rendering "Secrets" Page`); 
+            res.render(`secrets`);
+        }
+    })
 }) ; 
 
 // -* Listener
