b4c195e1a67526f5709fee340170d1cbf76d9c5a
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-05-29 03:39:48 +0800

    Clean up + Updated Routes + Add README.md
    1. Included a quick README.md for the project
    	1.1. I haven't gone too in-depth into it and I still intend to update it along the way, most likely once the project is done
    2. Cleaned up the app.js a ton
    	2.1. Since adding 4 new npm dependencies (express-session, passport, passport-local and passport-local-mongoose), the bloat and mess has been very noticeable
    	2.2. I actually want to compartmentalize some stuff by having a JS file just for the requirings and the configurations while the app.js file only handles the routings, but I'm not sure exactly how I would do that in a way that actually is easy and doesn't require too much work
    		2.2.1. I'll revisit this somewhere down the line and have taken note in my Notion project
    	2.3. For now, what I did was do some "proper" groupings of the code before the Express routes, as those are the clearly chaotic parts
    	2.4. This way, I managed to eliminate some obsolete code (such as the older dependencies that have been rendered moot by the new 4, mostly the encryption ones like bcrypt and md5 etc) AND I also had a better understanding of how to config the 4 new dependencies in collaboration with my already existing and well-understood code
    		2.4.1. I could clearly do well with creating a template for these types of app, which require login+registration and sessions and have included it in my Notion todos
    3. Beyond the clean-up, I also did the following for the routes
    	4.1. Created GET /secrets and GET /logout routes
    	4.2. Updated POST /register and POST /login routes to reflect the new way of doing things with the 4 new dependencies
    4. Next is I ought to clean-up my routings, as there are some dead-ends that I'm not so keen on

--- app.js
+++ app.js
@@ -1,61 +1,63 @@
-//jshint esversion:6
-// * Importing App NPM Dependencies
-require(`dotenv`).config();
-const express = require(`express`);
-const bodyParser = require(`body-parser`);
-const ejs = require(`ejs`);
-const mongoose = require(`mongoose`);
-const encrypt = require(`mongoose-encryption`);
-const md5 = require(`md5`);
-const bcrypt = require(`bcrypt`);
-const session = require(`express-session`);
-const passport = require(`passport`);
-const passportLocalMongoose = require(`passport-local-mongoose`);
+// ! ----------------------------------------
+// * SETUP EXPRESS
+// -* Requiring dependencies
+require(`dotenv`).config(); // Utilizing Environment Variables
+const express = require(`express`); // Primary back-end framework
+const bodyParser = require(`body-parser`); // Parsing POST requests
+const ejs = require(`ejs`); // Template engine for HTML rendering
 
-// * Setting up Express
-const app = express(); // Setting up app head
+// -* Configurations
+const app = express(); // Setting up app object
 app.use(express.static(`public`)); // Assigning default public path
-app.set(`view engine`, `ejs`); // Setting EJS as view engine
+app.set(`view engine`, `ejs`); // ? Setting EJS as view engine
 app.use(bodyParser.urlencoded({
     extended: true
-})); // For parsing req.body data from POST forms
+})); // ? For parsing req.body data from POST forms
 const port = 3000; // Port number; Adjustable depending on eventual hosting config
 
-// ! Proper Docu Needed
-app.use(session({
-    secret: "Our little secret.",
-    resave: false,
-    saveUninitialized: false
-}));
+// ! ----------------------------------------
+// * SETUP MONGODB + MONGOOSE
+// -* Requiring dependencies
+const mongoose = require(`mongoose`); // Handling MongoDB requests
 
-app.use(passport.initialize());
-app.use(passport.session());
-// ! Proper Docu Needed
-
-// * Setting up MongoDB
+// -* Configurations
 mongoose.connect(`mongodb://127.0.0.1:27017/secretsDB`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}); // Establishing connection to local MongoDB instance
+}); // ? Establishing connection to local MongoDB instance
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 }); // Defining the user schema
 
-// * Setting up Mongoose-Encryption
-const secret = process.env.SECRET; // ? This is the secret key, I believe
-userSchema.plugin(encrypt, {
-    secret: secret,
-    encryptedFields: ["password"]
-}); // Setting up the encryption plugin within the userSchema so that any save or find done on the model will trigger mongoose-encryption's methods
-// This only affects the "password" field as indicated
-userSchema.plugin(passportLocalMongoose); // ! Proper Docu Needed
-const User = new mongoose.model(`User`, userSchema); // Creating a new collection "User" using the user schema
-passport.use(User.createStrategy()); // ! Proper Docu Needed
-passport.serializeUser(User.serializeUser()); // ! Proper Docu Needed
-passport.deserializeUser(User.deserializeUser()); // ! Proper Docu Needed
-const saltRounds = 10;
+// ! ----------------------------------------
+// * SETUP EXPRESS-SESSION + PASSPORT-LOCAL-MONGOOSE
+// -* Requiring dependencies
+const session = require(`express-session`);
+const passport = require(`passport`);
+const passportLocalMongoose = require(`passport-local-mongoose`);
 
+// -* Configurations
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+})); // Setting up my express app to utilize sessions
+
+app.use(passport.initialize()); // Initializing passport with express
+app.use(passport.session()); // Needed for persistent login sessions
+
+userSchema.plugin(passportLocalMongoose); // Including the passport-local-mongoose package into the schema for automatic encryption of essential credentials
+const User = new mongoose.model(`User`, userSchema); // Creating a new collection "User" using the user schema
+passport.use(User.createStrategy()); // TODO Not sure
+passport.serializeUser((user, done) =>{
+    done(null, user);
+  }); // TODO Not sure
+passport.deserializeUser((user, done) =>{
+    done(null, user);
+  }); // TODO Not sure
+
+// ! ----------------------------------------
 // * EXPRESS ROUTES
 // -* GET Home
 app.get(`/`, function (req, res) {
@@ -84,12 +86,56 @@ app.get(`/register`, function (req, res) {
     res.render(`register`);
 });
 
+// -* GET Secrets
+app.get(`/secrets`, function (req, res) {
+    console.log(`\n`);
+    console.log(`GET /secrets`);
+
+    if (req.isAuthenticated()) {
+        console.log(`-User is logged in`);
+        console.log(`-Rendering secrets page`);
+        res.render(`secrets`);
+    } else {
+        console.log(`-User is not logged in`);
+        console.log(`-Redirecting to GET /login`);
+        res.redirect(`/login`);
+    }
+});
+
+// -* GET Logout
+app.get(`/logout`, function(req, res) {
+    console.log(`\n`);
+    console.log(`GET /register`);
+    
+    req.logout((err)=> {
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`);
+            console.log(err);
+            res.redirect(`/secrets`);
+        } else {
+            res.redirect(`/`);
+        }
+    });
+}) ; 
+
 // -* POST Register
 app.post(`/register`, function (req, res) {
     console.log(`\n`);
     console.log(`POST /register`);
 
-
+    User.register({
+        username: req.body.username
+    }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`);
+            console.log(err);
+            res.redirect(`/register`);
+        } else {
+            passport.authenticate(`local`)(req, res, () => {
+                res.redirect(`/secrets`);
+            });
+        }
+    });
 });
 
 // -* POST Login
@@ -97,6 +143,22 @@ app.post(`/login`, function (req, res) {
     console.log(`\n`);
     console.log(`POST /login`);
 
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, (err) => {
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`);
+            console.log(err);
+            res.redirect(`/login`);
+        } else {
+            passport.authenticate(`local`)(req, res, () => {
+                res.redirect(`/secrets`);
+            });
+        }
+    })
 });
 
 // -* Listener
