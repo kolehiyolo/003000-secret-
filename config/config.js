85ec6d1be5555f1dff71e90f50e68461a2ae7616
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-12 20:59:52 +0800

    Config and Routes Modularized
    1. The app config code is in config.js
    2. The routes are under routes.js

---
+++ config/config.js
@@ -0,0 +1,72 @@
+const express = require(`express`); // Primary back-end framework
+const bodyParser = require(`body-parser`); // Parsing POST requests
+const ejs = require(`ejs`); // Template engine for HTML rendering
+const app = express(); // Setting up app object
+
+app.use(express.static(`public`)); // Assigning default public path
+app.set(`view engine`, `ejs`); // ? Setting EJS as view engine
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // ? For parsing req.body data from POST forms
+
+// ! ----------------------------------------
+// * SETUP MONGODB + MONGOOSE
+// -* Requiring dependencies
+const mongoose = require(`mongoose`); // Handling MongoDB requests
+
+// -* Configurations
+mongoose.connect(`mongodb://127.0.0.1:27017/secretsDB`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); // ? Establishing connection to local MongoDB instance
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+}); // Defining the user schema
+
+// ! ----------------------------------------
+// * SETUP EXPRESS-SESSION + PASSPORT-LOCAL-MONGOOSE
+// -* Requiring dependencies
+const session = require(`express-session`);
+const passport = require(`passport`);
+const passportLocalMongoose = require(`passport-local-mongoose`);
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require(`mongoose-findorcreate`);
+
+// -* Configurations
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+})); // Setting up my express app to utilize sessions
+
+app.use(passport.initialize()); // Initializing passport with express
+app.use(passport.session()); // Needed for persistent login sessions
+
+userSchema.plugin(passportLocalMongoose); // Including the passport-local-mongoose package into the schema for automatic encryption of essential credentials
+userSchema.plugin(findOrCreate); // TODO Not sure
+const User = new mongoose.model(`User`, userSchema); // Creating a new collection "User" using the user schema
+passport.use(User.createStrategy()); // TODO Not sure
+
+passport.use(
+    new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+},   function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+}); // TODO Not sure
+passport.deserializeUser((user, done) => {
+    done(null, user);
+}); // TODO Not sure
+
+module.exports = {
+    app: app,
+    User: User
+};
---
+++ routes/routes.js
@@ -0,0 +1,138 @@
+const router = require(`express`).Router();
+const passport = require('passport');
+const User = require('../config/config').User;   
+
+// ! ----------------------------------------
+// * EXPRESS ROUTES
+// -* GET Home
+router.get(`/`, function (req, res) {
+    console.log(`\n`);
+    console.log(`GET /`);
+
+    console.log(`-Confirming if User is logged in to determine response`);
+    if (req.isAuthenticated()) {
+        console.log(`-User is logged in`);
+        console.log(`-Redirecting to GET /secrets`);
+        res.redirect(`/secrets`);
+    } else {
+        console.log(`-User is not logged in`);
+        console.log(`-Rendering home page`);
+        res.render(`home`);
+    }
+});
+
+// -* auth with google+
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+router.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+});
+
+router.get('/auth/google/secrets', function (req, res) {
+    passport.authenticate('google', {
+        failureRedirect: '/login'
+    })(req, res, () => {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
+});
+
+// -* GET Login
+router.get(`/login`, function (req, res) {
+    console.log(`\n`);
+    console.log(`GET /login`);
+    console.log(`-Rendering login page`);
+
+    res.render(`login`);
+});
+
+// -* GET Register
+router.get(`/register`, function (req, res) {
+    console.log(`\n`);
+    console.log(`GET /register`);
+    console.log(`-Rendering register page`);
+
+    res.render(`register`);
+});
+
+// -* GET Secrets
+router.get(`/secrets`, function (req, res) {
+    console.log(`\n`);
+    console.log(`GET /secrets`);
+
+    if (req.isAuthenticated()) {
+        console.log(`-User is logged in`);
+        console.log(`-Rendering secrets page`);
+        res.render(`secrets`);
+    } else {
+        console.log(`-User is not logged in`);
+        console.log(`-Redirecting to GET /login`);
+        res.redirect(`/login`);
+    }
+});
+
+// -* GET Logout
+router.get(`/logout`, function (req, res) {
+    console.log(`\n`);
+    console.log(`GET /register`);
+
+    req.logout((err) => {
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`);
+            console.log(err);
+            res.redirect(`/secrets`);
+        } else {
+            res.redirect(`/`);
+        }
+    });
+});
+
+// -* POST Register
+router.post(`/register`, function (req, res) {
+    console.log(`\n`);
+    console.log(`POST /register`);
+
+    User.register({
+        username: req.body.username
+    }, req.body.password, (err, user) => {
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`);
+            console.log(err);
+            res.redirect(`/register`);
+        } else {
+            passport.authenticate(`local`)(req, res, () => {
+                res.redirect(`/secrets`);
+            });
+        }
+    });
+});
+
+// -* POST Login
+router.post(`/login`, function (req, res) {
+    console.log(`\n`);
+    console.log(`POST /login`);
+
+    const user = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    req.login(user, (err) => {
+        if (err) {
+            console.log(`-ERROR ENCOUNTERED:`);
+            console.log(err);
+            res.redirect(`/login`);
+        } else {
+            passport.authenticate(`local`)(req, res, () => {
+                res.redirect(`/secrets`);
+            });
+        }
+    })
+});
+
+module.exports = router;
